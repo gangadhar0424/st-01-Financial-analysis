f0da90cd100f6183088867849d09fcbe03b0c996
Harini Nallam <nallam.harini@gmail.com>
2025-02-05 12:42:46 +0530

    homepage

---
+++ templates/script.js
@@ -0,0 +1,4 @@
+document.getElementById('predictButton').addEventListener('click', function() {
+    alert('Predict Sentiment button clicked!');
+    // You can add more functionality here, like redirecting to another page or showing a modal.
+});
