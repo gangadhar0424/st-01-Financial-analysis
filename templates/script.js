16002b5144485fb33370dd2886b0cd271d567b50
Mamatha Nagisetty <mamathanagisetty88977@gmail.com>
2025-02-05 12:48:12 +0530

    app

---
+++ templates/script.js
@@ -0,0 +1,4 @@
+// Add interactivity to the Predict Sentiment button
+document.getElementById('predictButton').addEventListener('click', () => {
+    alert('Predict Sentiment feature coming soon!');
+  });
