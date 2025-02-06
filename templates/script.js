50c2e0d0c1ab09982d5ffad0e8864b9416a2932b
Harini Nallam <nallam.harini@gmail.com>
2025-02-06 19:37:35 +0530

    nlp code

--- templates/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('predictButton').addEventListener('click', function() {
-    alert('Predict Sentiment button clicked!');
-    // You can add more functionality here, like redirecting to another page or showing a modal.
-});
