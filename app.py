da3badb549b1068b4a4d5762342f3fb91ba531f1
Mamatha Nagisetty <mamathanagisetty88977@gmail.com>
2025-02-14 12:39:44 +0530

    final code

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def predict():
         return render_template("predict.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5050', debug=True)
+    app.run(host='0.0.0.0', port='5050')
